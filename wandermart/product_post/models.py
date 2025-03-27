406447c79e516d5160c15ece6d6b57398da88df8
ShizunaBSIT <msyokozawa@tip.edu.ph>
2025-03-27 17:50:15 +0800

    Initialize storefront

--- wandermart/product_post/models.py
+++ wandermart/product_post/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from storefront.models import storefront
 
-# Create your models here.
+# product listing status choices
+PRODUCT_LISTING_STATUS_CHOICES = (
+    ('draft','Draft'),
+    ('published','Published'),
+    ('archived','Archived'),
+) 
+
+# Product Listing
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    description = models.TextField()
+    store = models.ForeignKey(storefront, on_delete=models.CASCADE)
+    stock = models.BigIntegerField()
+    dateadded = models.DateField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True) 
+    page = models.SlugField(max_length=50, unique_for_date=dateadded)
+    status = models.CharField(max_length=10, choices=PRODUCT_LISTING_STATUS_CHOICES, default='draft')
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        ordering = ['price']
---
+++ wandermart/storefront/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+STORE_STATUS_CHOICES = (
+    ('open','Open'),
+    ('closed','Closed'),
+    ('removed','Removed'),
+)
+
+# storefront
+class storefront(models.Model):
+    owner = models.ForeignObject()
+    storename = models.CharField(max_length=100, unique=True)
+    address = models.TextField(default="No Physical Store")
+    phonecontact = models.CharField(max_length=11)
+    emailcontact = models.CharField(max_length=100)
+    slugpage = models.SlugField()
+    storestatus = models.TextChoices(STORE_STATUS_CHOICES, default='open')
+    datecreated = models.DateField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return f"{self.storename}"
+    
+    class Meta:
+        ordering = []
