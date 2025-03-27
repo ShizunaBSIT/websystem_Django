10a3f377f4d7f7d3356b4800e892a1e6c6803b78
ShizunaBSIT <msyokozawa@tip.edu.ph>
2025-03-27 17:57:33 +0800

    update admin

--- wandermart/product_post/admin.py
+++ wandermart/product_post/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from models import Product
 
 # Register your models here.
+class postAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Product, postAdmin)
--- wandermart/storefront/admin.py
+++ wandermart/storefront/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from models import storefront
 
 # Register your models here.
+class storefrontAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(storefront, storefrontAdmin)
