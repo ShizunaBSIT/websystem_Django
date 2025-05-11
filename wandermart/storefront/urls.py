eadc999407ae815bf6b21afeee484b7bf159fc93
Zanzanzanan <mznpingol@tip.edu.ph>
2025-05-11 19:15:30 +0800

    Added design on storefront

--- wandermart/storefront/urls.py
+++ wandermart/storefront/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name = 'product_post'
+app_name = 'storefront'
 
 urlpatterns = [
     path('', views.store_list, name='store_list'),
     #path('<int:year>/<int:month>/<int:day>/<slug:product>/', views.product_detail, name='product_detail'),
-    
+    path('<slug:store_slug>/', views.store_page, name='store_page'),
 ]
